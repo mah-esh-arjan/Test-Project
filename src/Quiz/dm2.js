a75224bd545de1c52f5cc1ea640296ad41244c48
mah-esh-arjan <mah.dot98@gmail.com>
2025-01-03 10:45:51 +0545

    dm2 Qn 2 & 3

--- src/Quiz/dm2.js
+++ src/Quiz/dm2.js
@@ -1,13 +1,82 @@
-const [cart,setCart] = useState([]);
 
-// [{name: "laptop",key: value}]
-function MergeCart(cart1, cart2){
 
-    value1 = Object.values(cart1)
-    value2 = Object.values(cart2)
+function mergeCart(cart1, cart2) {
+    
+    for (key in cart1){   //1 , 2 
+        if(cart2[key]){ //cart2[key] represents an whole object 1:{name:"lorem",age:"ipsum"}
+            cart1[key].quantity = cart1[key].quantity + cart2[key].quantity;
+            continue;
+        }
+    }
+    for (key in cart2){
+        if(!cart1[key]){
+            cart1[key] = cart2[key]
+        }
+    }
 
-    setCart((prevCart) =>[...prevCart,
-        ...value1,
-        ...value2
-    ])
+    return cart1 ;
+}
+
+const cart1 = { 1: { name: "Laptop", quantity: 1, price: 1000 },
+                2: { name: "Phone", quantity: 1, price: 800 } };
+const cart2 = { 2: { name: "Phone", quantity: 1, price: 800 } };
+
+
+//qn2
+const handleCartArray = () => {
+
+    setCart((prevCart) => prevCart.map((item) => (
+    {    ...item,
+        price: item.price * 0.9}
+    )))
+
+}
+
+const handleCartQuantity = () => {
+ setCart((prevCart) => prevCart.map((item) => ({
+        ...item,
+        quantity: item.quantity+1
+    })))
+}
+
+//qn3
+const handleWarranty = () => {
+
+    setCart( (prevCart) => {
+        let updatedCart = {};
+
+        for ( key in prevCart){
+            updatedCart[key] = {
+                ...prevCart[key],
+                    details: {
+                    ...prevCart[key].details,
+                    warranty : "3 years"
+                }
+            }
+
+        }
+
+        return updatedCart;
+
+    })
+
+}
+
+const handleAddDetail = () => {
+    setCart( (prevCart) => {
+        let addDetailObj = {};
+
+        for (key in prevCart){
+            addDetailObj[key] = {
+                ...prevCart[key],
+                details: {
+                    ...prevCart[key].details,
+                    brand: "apple",
+                }
+            }
+
+        }
+        return addDetailObj;
+    })
+
 }
